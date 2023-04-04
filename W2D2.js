e9db3267cfb5f9f89c4520689a13768149904a0f
Tesfaye Hailu <thailu@LABA.PSWAS.LOCAL>
2023-04-04 10:06:51 -0700

    w2d2_Exercises

---
+++ W2D2.js
@@ -0,0 +1,15 @@
+let greetings = "Hello";
+
+const displaygreetings = () => {
+  console.log(greetings);
+}
+
+displaygreetings(); 
+
+
+const calculateArea = (l,w) => {
+    let area = l * w;
+    console.log(area);
+  }
+  
+  calculateArea(5,4); // logs 20 to the console
